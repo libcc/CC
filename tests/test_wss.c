bd5ad0b7b3736ec3eceeb8ae8713de501d6213d3
huai <huai2001@outlook.com>
2025-09-25 14:40:23 +0800

    Update test_wss.c

--- tests/test_wss.c
+++ tests/test_wss.c
@@ -4,18 +4,18 @@
 
 _cc_OpenSSL_t *openSSL;
 
-typedef struct _WebSocket {
+typedef struct _ws {
     uint8_t status;
     uint8_t handshake;
-    _cc_http_request_header_t *request;
-    _cc_buf_t buffer;
     _cc_io_buffer_t *io;
+    _cc_http_request_header_t *request;
+    int64_t raw_length;
     int64_t payload;
-    char_t websocket_key[256];
-} _WebSocket_t;
+    char_t hash[256];
+} _ws_t;
 
 /**/
-_CC_API_PRIVATE(void) _WebSocketSecKey(const tchar_t *sec_websocket_key, _WebSocket_t *ws) {
+_CC_API_PRIVATE(void) _WSKey(const tchar_t *sec_websocket_key, _ws_t *ws) {
     _cc_sha1_t ctx;
     size_t length;
     byte_t results[1024];
@@ -27,32 +27,34 @@ _CC_API_PRIVATE(void) _WebSocketSecKey(const tchar_t *sec_websocket_key, _WebSoc
     _cc_sha1_init(&ctx);
     _cc_sha1_update(&ctx, results, length);
     _cc_sha1_final(&ctx, sha1_results);
-    _cc_base64_encode(sha1_results, _CC_SHA1_DIGEST_LENGTH_, ws->websocket_key, _cc_countof(ws->websocket_key));
+    _cc_base64_encode(sha1_results, _CC_SHA1_DIGEST_LENGTH_, ws->hash, _cc_countof(ws->hash));
 }
 
 /**/
-_CC_API_PRIVATE(bool_t) _WebSocketResponseHeader(_cc_event_t *e, _WebSocket_t *ws) {
+_CC_API_PRIVATE(bool_t) _WSResponseHeader(_cc_event_t *e, _ws_t *ws) {
+    const _cc_http_header_t *data = _cc_http_header_find(&ws->request->headers, _T("Sec-WebSocket-Protool"));
+    _cc_sds_t protool = data ? data->value : _T("JSON");
     _cc_io_buffer_t *io = (_cc_io_buffer_t*)ws->io;
     io->w.off = _snprintf((char*)io->w.bytes, io->w.limit,
                         "HTTP/1.1 101 Switching Protocols\r\n"
                         "Connection: Upgrade\r\n"
                         "Upgrade: websocket\r\n"
-                        "Sec-WebSocket-Protool: chat\r\n"
+                        "Sec-WebSocket-Protool: %s\r\n"
                         "Sec-WebSocket-Accept: %s\r\n\r\n",
-                        ws->websocket_key);
+                        protool, ws->hash);
 
     //printf("send: %.*s\n",io->w.off, io->w.bytes);
     return _cc_io_buffer_flush(e, io);
 }
 
-_CC_API_PRIVATE(int64_t) _WebSocketGetContentLength(_cc_rbtree_t *headers) {
+_CC_API_PRIVATE(int64_t) _WSGetContentLength(_cc_rbtree_t *headers) {
     const _cc_http_header_t *data = _cc_http_header_find(headers, _T("Content-Length"));
     return data ? _ttoi(data->value) : 0;
 }
 
 /**/
-_CC_API_PRIVATE(bool_t) _Heartbeat(_cc_event_t *e, byte_t oc) {
-    _WebSocket_t *ws = (_WebSocket_t*)e->data;
+_CC_API_PRIVATE(bool_t) _WSHeartbeat(_cc_event_t *e, byte_t oc) {
+    _ws_t *ws = (_ws_t*)e->data;
     _cc_io_buffer_t *io = (_cc_io_buffer_t*)ws->io;
     byte_t buf[2];
     buf[0] = 0x80 | oc;
@@ -61,11 +63,10 @@ _CC_API_PRIVATE(bool_t) _Heartbeat(_cc_event_t *e, byte_t oc) {
 }
 
 /**/
-_CC_API_PRIVATE(void) _WebSocketFree(_WebSocket_t *ws) {
+_CC_API_PRIVATE(void) _WSFree(_ws_t *ws) {
     if (ws->request) {
         _cc_http_free_request_header(&ws->request);
     }
-    _cc_free_buf(&ws->buffer);
     if (ws->io) {
         _cc_free_io_buffer(ws->io);
     }
@@ -73,8 +74,8 @@ _CC_API_PRIVATE(void) _WebSocketFree(_WebSocket_t *ws) {
 }
 
 /**/
-_CC_API_PRIVATE(bool_t) _WebSocketData(_cc_event_t *e) {
-    _WebSocket_t *ws = (_WebSocket_t*)e->data;
+_CC_API_PRIVATE(bool_t) _WSData(_cc_event_t *e) {
+    _ws_t *ws = (_ws_t*)e->data;
     _cc_io_buffer_t *io = (_cc_io_buffer_t*)ws->io;
     _CCWSHeader_t header = {0};
 
@@ -92,7 +93,7 @@ _CC_API_PRIVATE(bool_t) _WebSocketData(_cc_event_t *e) {
             //fn(header, header.bytes + header.offset, header.payload);
             switch (header.opc) {
                 case WS_OP_PING:
-                    _Heartbeat(e, WS_OP_PONG);
+                    _WSHeartbeat(e, WS_OP_PONG);
                     break;
                 case WS_OP_PONG:
                     break;
@@ -130,13 +131,20 @@ _CC_API_PRIVATE(bool_t) _WebSocketData(_cc_event_t *e) {
     return true;
 }
 
+static void bad_request(_cc_event_t *e, _cc_io_buffer_t *io) {
+    _cc_String_t body = _cc_String("<HTML><HEAD><TITLE>BAD REQUEST</TITLE></HEAD><BODY><P>Your browser sent a bad request, such as a POST without a Content-Length.</P></BODY></HTML>");
+
+    io->w.off = snprintf((char*)io->w.bytes, io->w.limit,"HTTP/1.1 400 BAD REQUEST\r\nConnection: close;\r\nContent-type: text/html\r\nContent-Length: %ld\r\n\r\n", body.length);
+    memcpy(io->w.bytes + io->w.off, body.data, body.length * sizeof(char_t));
+    io->w.off += (int32_t)body.length * sizeof(char_t);
+    _cc_io_buffer_flush(e, io);
+}
+
 static bool_t network_event_callback(_cc_async_event_t *async, _cc_event_t *e, const uint32_t which) {
-    _WebSocket_t *ws = (_WebSocket_t*)e->data;
+    _ws_t *ws = (_ws_t*)e->data;
     if (which & _CC_EVENT_ACCEPT_) {
         _cc_event_t *event;
         _cc_socket_t fd;
-        uint32_t timeout = e->timeout;
-        uint32_t flags = _CC_EVENT_TIMEOUT_;
         struct sockaddr_in remote_addr = {0};
         _cc_socklen_t remote_addr_len = sizeof(struct sockaddr_in);
 
@@ -148,44 +156,31 @@ static bool_t network_event_callback(_cc_async_event_t *async, _cc_event_t *e, c
 
         _cc_set_socket_nonblock(fd, 1);
 
-        ws = (_WebSocket_t*)_cc_malloc(sizeof(_WebSocket_t));
-        ws->buffer.length = 0;
-        ws->buffer.limit = 0;
-        ws->buffer.bytes = nullptr;
-        ws->request = nullptr;
-        ws->payload = 0;
-        ws->io = _cc_alloc_io_buffer(_CC_IO_BUFFER_SIZE_);
+        ws = (_ws_t*)_cc_malloc(sizeof(_ws_t));
+        ws->handshake = _CC_SSL_HS_SYSCALL_WOULDBLOCK_;
         ws->status = _CC_HTTP_STATUS_HEADER_;
-        ws->io->ssl = _SSL_accept(openSSL, fd, &ws->handshake);
+        ws->raw_length = 0;
+        ws->payload = 0;
+        ws->request = nullptr;
+        ws->io = _cc_alloc_io_buffer(_CC_IO_BUFFER_SIZE_);
+        ws->io->ssl = _SSL_accept(openSSL, fd);
 
-        if (ws->handshake == _CC_SSL_HS_ERROR_) {
-            _cc_logger_error(_T("SSL accept fail %s."), _cc_last_error(_cc_last_errno()));
-            _WebSocketFree(ws);
-            _cc_close_socket(fd);
-            return true;
-        } else if (ws->handshake == _CC_SSL_HS_ESTABLISHED_) {
-            flags |= _CC_EVENT_READABLE_;
-        } else {
-            //wait for handshake
-            timeout = 1000;
-        }
-
-        event = _cc_event_alloc(async, flags);
+        event = _cc_event_alloc(async, _CC_EVENT_TIMEOUT_);
         if (event == nullptr) {
             _cc_close_socket(fd);
-            _WebSocketFree(ws);
+            _WSFree(ws);
             return true;
         }
 
         event->fd = fd;
         event->callback = e->callback;
-        event->timeout = timeout;
+        event->timeout = 100; //wait SSL handshake complete
         event->data = (uintptr_t)ws;
 
         if (async->attach(async, event) == false) {
             _cc_logger_debug(_T("thread %d add socket (%d) event fial."), _cc_get_thread_id(nullptr), fd);
             _cc_free_event(async, event);
-            _WebSocketFree(ws);
+            _WSFree(ws);
             return true;
         }
 
@@ -196,12 +191,30 @@ static bool_t network_event_callback(_cc_async_event_t *async, _cc_event_t *e, c
         }
 
         return true;
-    }
-
-    if (which & _CC_EVENT_CLOSED_) {
+    } else if (which & _CC_EVENT_TIMEOUT_) {
+#ifdef _CC_USE_OPENSSL_
+        if (ws->handshake != _CC_SSL_HS_ESTABLISHED_) {
+            ws->handshake = _SSL_do_handshake(ws->io->ssl);
+            if (ws->handshake == _CC_SSL_HS_ESTABLISHED_) {
+                e->timeout = 60000;
+                _CC_SET_BIT(_CC_EVENT_READABLE_, e->flags);
+                return true;
+            } else if (ws->handshake == _CC_SSL_HS_ERROR_) {
+                return false;
+            }
+            //wait SSL handshake complete
+            return true;
+        }
+#endif
+        _cc_logger_debug(_T("TCP timeout %d"), e->fd);
+        if (_WSHeartbeat(e, WS_OP_PONG)) {
+            return true;
+        }
+        return false;
+    } else if (which & _CC_EVENT_CLOSED_) {
         _cc_logger_debug(_T("%d disconnect to client."), e->fd);
         if (e->data) {
-            _WebSocketFree((_WebSocket_t*)e->data);
+            _WSFree((_ws_t*)e->data);
         }
         return false;
     }
@@ -217,7 +230,7 @@ static bool_t network_event_callback(_cc_async_event_t *async, _cc_event_t *e, c
         }
 
         if (ws->status == _CC_HTTP_STATUS_ESTABLISHED_) {
-            if(_WebSocketData(e)) {
+            if(_WSData(e)) {
                 return true;
             }
             return false;
@@ -231,36 +244,36 @@ static bool_t network_event_callback(_cc_async_event_t *async, _cc_event_t *e, c
                 return ws->status == _CC_HTTP_STATUS_HEADER_;
             }
 
-            ws->payload = _WebSocketGetContentLength(&ws->request->headers);
+            ws->payload = _WSGetContentLength(&ws->request->headers);
             if (ws->payload == 0) {
                 ws->status = _CC_HTTP_STATUS_ESTABLISHED_;
             }
 
-            if (ws->buffer.bytes == nullptr && ws->payload > 0) {
-                _cc_alloc_buf(&ws->buffer, (size_t)ws->payload);
-            }
             connection = _cc_http_header_find(&ws->request->headers,_T("Connection"));
             upgrade = _cc_http_header_find(&ws->request->headers, _T("Upgrade"));
             sec_websocket_key = _cc_http_header_find(&ws->request->headers, _T("Sec-WebSocket-Key"));
             if (connection == nullptr || upgrade == nullptr || sec_websocket_key == nullptr) {
+                bad_request(e, io);
+                return false;
+            } else if (_tcsicmp("Upgrade",connection->value) != 0 || _tcsicmp("websocket",upgrade->value) != 0) {
+                bad_request(e, io);
                 return false;
             }
-            if (_tcsicmp("Upgrade",connection->value) != 0 || _tcsicmp("websocket",upgrade->value) != 0) {
-                return false;
-            }
-            _WebSocketSecKey(sec_websocket_key->value, ws);
+
+            _WSKey(sec_websocket_key->value, ws);
         } 
 
         if (ws->status == _CC_HTTP_STATUS_PAYLOAD_) {
-            _cc_buf_append(&ws->buffer, io->r.bytes, io->r.off);
-            if (ws->buffer.length >= ws->payload) {
+            _cc_logger_debug(_T("%.*s."), io->r.off,io->r.bytes);
+            if (ws->raw_length >= ws->payload) {
                 ws->status = _CC_HTTP_STATUS_ESTABLISHED_;
             }
+            ws->raw_length += io->r.off;
             io->r.off = 0;
         }
 
         if (ws->status == _CC_HTTP_STATUS_ESTABLISHED_) {
-            return _WebSocketResponseHeader(e,ws);
+            return _WSResponseHeader(e,ws);
         }
 
         return true;
@@ -270,25 +283,9 @@ static bool_t network_event_callback(_cc_async_event_t *async, _cc_event_t *e, c
 
     }
 
-    if (which & _CC_EVENT_TIMEOUT_) {
-        if (ws->handshake != _CC_SSL_HS_ESTABLISHED_) {
-            ws->handshake = _SSL_do_handshake(ws->io->ssl);
-            _cc_logger_debug(_T("SSL do handshake %d"), ws->handshake);
-            if (ws->handshake == _CC_SSL_HS_ESTABLISHED_) {
-                e->flags |= _CC_EVENT_READABLE_;
-                e->timeout = 60000;
-            }
-            return true;
-        }
-        if (_Heartbeat(e, WS_OP_PONG)) {
-            return true;
-        }
-        _cc_logger_debug(_T("TCP timeout %d"), e->fd);
-        return false;
-    }
-
     return true;
 }
+
 int main(int argc, char *const argv[]) {
     // char c = 0;
     struct sockaddr_in sa;
